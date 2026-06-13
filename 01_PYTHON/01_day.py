248e6c4b3ff203d1b9857dc95d8fa89dd685f6eb
rahul8879 <rtiwarirahul123@gmail.com>
2026-06-13 20:19:22 +0530

    added flow control basic code

--- 01_PYTHON/01_day.py
+++ 01_PYTHON/01_day.py
@@ -1,4 +1,43 @@
-x = input("Enter a number: ")
-y = input("Enter another number: ")
-print(int(x) + int(y))
-print('one more changes')
+age = '34'
+# print(age+5)
+is_married = True
+print(type(is_married))
+print(type(age))
+
+# variables ??
+# placeholder -->It will hold ypur data --> you can do some operation
+# 1_test = 34
+# 1. Variable name cant start with number/digit
+test_1 = 34
+print(test_1) 
+
+# 2. Variable name cant start with special character( @ , # , $ , % , & , * , ! , _ , - , + , = , / , \ , | , ~ , ` , ' , " , ? , , , . , ; , : , < , > , )
+# $_test= 34
+
+# 3. You cant use reserved keywords as a variable name 
+# you can refer here https://www.w3schools.com/python/python_ref_keywords.asp
+
+# name = 'rahul'
+# print(name)
+# print(len(name))
+
+
+# you can select multiple line and then press "ctrl + /" to comment multiple line
+# ssn_number = input('Enter your ssn number : ')
+# print(ssn_number)
+# print(type(ssn_number))
+# print(len(ssn_number))
+
+# if len(ssn_number)==10:
+#     print('hello')
+#     print('how')
+#     print('are')
+#     print('you')
+# else:
+#     print('I am here')
+
+
+
+
+
+
